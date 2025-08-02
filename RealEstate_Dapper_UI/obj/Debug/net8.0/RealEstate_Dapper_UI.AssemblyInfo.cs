b536f023cf74522dc2c27e941d21c31664f27e54
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-08-02 21:56:04 +0300

    _MemberLayout ve ProfileController projeye eklendi.

--- RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
+++ RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3748ae851ff58f1cbcdeb8469b2e79f6c4e84bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
