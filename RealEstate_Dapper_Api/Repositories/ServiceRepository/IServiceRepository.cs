1daddc61a43966f4046f8c5613170aaa8cf0e37a
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-08-06 20:57:15 +0300

    Service ve WhoWeAre kısmını dinamik olarak anasayfaya çekmek kısmı tamamlandı.

---
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/IServiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.Dtos.ServiceDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
+{
+    public interface IServiceRepository
+    {
+        Task<List<ResultServiceDto>> GetAllServiceAsync();
+        void CreateService(CreateServiceDto categoryDto);
+        void DeleteService(int id);
+        void UpdateService(UpdateServiceDto categoryDto);
+        Task<GetByIDServiceDto> GetCategory(int id);
+    }
+}
