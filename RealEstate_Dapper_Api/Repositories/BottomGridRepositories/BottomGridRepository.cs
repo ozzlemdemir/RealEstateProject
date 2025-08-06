6c434565b81118bf0a07f7b2ccd83b16e10345b8
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-08-06 22:12:37 +0300

    Anasayfada bulunan BottomGrid alanı dinamik hale getirildi.

---
+++ RealEstate_Dapper_Api/Repositories/BottomGridRepositories/BottomGridRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public class BottomGridRepository : IBottomGridRepository
+    {
+        private readonly Context _context;
+        public BottomGridRepository(Context context)
+        {
+            _context = context;
+        }
+        public void CreateBottomGrid(CreateBottomGridDto bottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultBottomGridDto>> GetAllBottomGridAsync()
+        {
+            string query = "Select * from BottomGrid";
+            var parameter = new DynamicParameters();
+            using (var conneciton = _context.CreateConnection())
+            {
+                var values = await conneciton.QueryAsync<ResultBottomGridDto>(query);
+                return values.ToList();
+
+            }
+            
+        }
+
+        public Task<GetBottomGridDto> GetBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBottomGrid(UpdateBottomGridDto bottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/BottomGridRepositories/IBottomGridRepository.cs
@@ -0,0 +1,14 @@
+﻿using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.ServiceDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public interface IBottomGridRepository
+    {
+        Task<List<ResultBottomGridDto>> GetAllBottomGridAsync();
+        void CreateBottomGrid(CreateBottomGridDto bottomGridDto);
+        void DeleteBottomGrid(int id);
+        void UpdateBottomGrid(UpdateBottomGridDto bottomGridDto);
+        Task<GetBottomGridDto> GetBottomGrid(int id);
+    }
+}
