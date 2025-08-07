ed9dd783b1a18cf3d1a8c84bb6259c260bf61964
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-08-07 21:35:23 +0300

    Popüler lokasyonlar ve Kullanıcı yorumları dinamik olarak anasayfaya çekildi.

---
+++ RealEstate_Dapper_Api/Controllers/PopularLocationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PopularLocationController : ControllerBase
+    {
+        private readonly IPopularLocationRepository _locationRepository;
+
+        public PopularLocationController(IPopularLocationRepository popularLocationRepository)
+        {
+            _locationRepository = popularLocationRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> PopularLocationList()
+        {
+            var value= await _locationRepository.GetAllPopularLocationAsync();
+            return Ok(value);
+        }
+    }
+}
