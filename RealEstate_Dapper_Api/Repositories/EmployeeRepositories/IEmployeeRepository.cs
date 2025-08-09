629ec263f0b6ae8de9f369e01d0a7fb273674a65
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-08-09 21:08:10 +0300

    Employee tablosunun API ile veri çekme ve UI kısmında consume etme kısmı tamamlandı.

---
+++ RealEstate_Dapper_Api/Repositories/EmployeeRepositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.EmployeeDros;
+
+namespace RealEstate_Dapper_Api.Repositories.EmployeeRepositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<ResultEmployeeDto>> GetAllEmployee();
+        void CreateEmployee(CreateEmployeeDto createEmployeeDto);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(UpdateEmployeeDto  updateEmployeeDto);
+        Task<GetByIDEmployeeDto> GetEmployee(int id);
+    }
+}
